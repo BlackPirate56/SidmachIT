f6a86f8808ebc5e1fc99bf228526366afbf991fc
BlackPirate56 <47182396+BlackPirate56@users.noreply.github.com>
2019-01-30 16:57:01 +0100

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿// Created a new controller to expose sign-in and sign-out methods
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+namespace Grade56_SSO.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        /* 
+        Added the following methods to handle sign-in and sign-out top your controller
+        by initiating and authenticating challenge via code.
+        */
+        /// <summary>
+        /// Send an OpenID Connect sign-in request
+        /// </summary>
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                new AuthenticationProperties { RedirectUri = "/" },
+                OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+
+        }
+
+        /// <summary>
+        /// Send an OpenID Connect sign-out request
+        /// </summary>
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+            OpenIdConnectAuthenticationDefaults.AuthenticationType,
+            CookieAuthenticationDefaults.AuthenticationType);
+        }
+    }
+}
